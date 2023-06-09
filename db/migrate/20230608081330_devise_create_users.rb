8269985c8c56406c1beabd723bc393da33c3aef9
TsuKi-3Moti <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-06-09 08:36:37 +0000

    [Add] devise_migration_file

--- db/migrate/20230608081330_devise_create_users.rb
+++ db/migrate/20230608081330_devise_create_users.rb
@@ -4,8 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: "", unique: true
+      t.string :encrypted_password, null: false, default: "", unique: true
 
       ## Recoverable
       t.string   :reset_password_token
@@ -32,6 +32,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :name, null: false
+      t.text   :introduction
+      t.boolean :is_deleted, null: false, default: false
 
       t.timestamps null: false
     end
