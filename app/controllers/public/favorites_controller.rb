9c515c86a408b32025d1466da81cebdaaf87d3e5
TsuKi-3Moti <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-06-11 09:49:44 +0000

    [Add] users relationships questions answers

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,15 +1,15 @@
 class Public::FavoritesController < ApplicationController
-  
+
   def create
   end
-  
-  def inde
-  end
-  
+
   def destroy
   end
-  
+
   def update
   end
-  
+
+  def favorited_answers
+  end
+
 end
