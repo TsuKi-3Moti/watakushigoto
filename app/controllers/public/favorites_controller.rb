2ac21e6b756b36c9d58ed4e44437ed6f44cf8418
TsuKi-3Moti <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-06-11 09:12:24 +0000

    [Add] create_view

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,2 +1,15 @@
 class Public::FavoritesController < ApplicationController
+  
+  def create
+  end
+  
+  def inde
+  end
+  
+  def destroy
+  end
+  
+  def update
+  end
+  
 end
---
+++ app/controllers/public/relationships_controller.rb
@@ -0,0 +1,12 @@
+class Public::RelationshipsController < ApplicationController
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
