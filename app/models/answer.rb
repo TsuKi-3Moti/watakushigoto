595ed0d6f323136607b26e7fd06eeb78c9ed387a
TsuKi-3Moti <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-06-12 15:31:09 +0000

    [Add] association

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,12 @@
 class Answer < ApplicationRecord
+
+  belongs_to :question
+
+  has_many :favorites, dependent: :destroy
+
+  has_many :relationships, foreign_key: "answer_id", dependent: :destroy
+  has_many :tags, through: :relationships
+  
+  validates :opinion, presence: true
+
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,6 @@
 class Tag < ApplicationRecord
+
+  has_many :relationships, foreign_key: "tag_id", dependent: :destroy
+  has_many :answers, through: :relationships
+
 end
