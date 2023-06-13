cfeeae2fdfd888655ebd19d59b10bc3ff3d81c1d
TsuKi-3Moti <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-06-13 15:28:52 +0000

    [Add] posting_function

--- app/controllers/public/questions_controller.rb
+++ app/controllers/public/questions_controller.rb
@@ -1,12 +1,27 @@
 class Public::QuestionsController < ApplicationController
 
   def create
+    @question = current_user.questions.new(question_params)
+    # @Question.user_id = current_user.id
+    if @question.save
+      redirect_to question_path(@question.id), notice: "You have created Question successfully."
+    else
+      @questions = Question.all
+      render :index
+    end
   end
 
   def show
+    @question = Question.find(params[:id])
+    @user = @question.user
+    @new_question = Question.new
+    @answer = Answer.new
   end
 
   def index
+    @questions = Question.all
+    @question = Question.new
+    @user = @question.user
   end
 
   def update
@@ -16,6 +31,12 @@ class Public::QuestionsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title)
   end
 
 end
