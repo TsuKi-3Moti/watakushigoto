5bd8e6312367d44fbf8bac5037a26fb82aa8394f
TsuKi-3Moti <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-06-16 10:45:18 +0000

    [Add] Asynchronous communication of favorite functions

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,15 @@ module Watakushigoto
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # タイムゾーンを日本時間に変更
+    config.time_zone = 'Tokyo'
+
     # デフォルト言語を英語から日本語に設定
     config.i18n.default_locale = :ja
-    
+
     # config/locales/以下に設定した翻訳ファイルを全て読み込めるようにする
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-    
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
