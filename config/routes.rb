712d1f9706dfb1b491a2e7375552b3256f868a42
TsuKi-3Moti <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-06-23 16:54:49 +0000

    [Add] guest-login

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
 
   scope module: :public do
 
+    devise_scope :user do
+      post "users/guest_sign_in", to: "sessions#guest_sign_in"
+    end
+
     root to:       "homes#top"
     get "about" => "homes#about", as: "about"
 
