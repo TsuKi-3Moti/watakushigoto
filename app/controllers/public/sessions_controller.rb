7d78fabc2bbc7633f4abe72261f36417b8df91f1
TsuKi-3Moti <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-06-24 22:58:25 +0000

    [Update] layout

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,12 +3,6 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  def guest_sign_in
-    user = User.guest
-    sign_in user
-    redirect_to user_path(user), notice: "guest userでログインしました"
-  end
-
   # GET /resource/sign_in
   # def new
   #   super
@@ -30,4 +24,19 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to user_path(user), notice: "guest userでログインしました"
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
 end
